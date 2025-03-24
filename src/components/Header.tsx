06bb641a5bd7948123f44226899a7fc246065511
Gaushu <xzanderjamesnollora@gmail.com>
2025-03-24 15:46:35 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ darkMode, title, subtitle }) => {
   return (
 <div
-  className={`fixed xl:top-[11em] top-[4em] sm:top-[4em] md:top-[7em] lg:top-[9em] border left-0 w-full py-1 text-center space-y-[-10px] xl:space-y-[-17px] z-50 ${
+  className={`sticky xl:top-[11em] top-[4em] sm:top-[4em] md:top-[7em] lg:top-[9em] left-0 w-full py-1 text-center space-y-[-10px] xl:space-y-[-17px] z-50 ${
     darkMode ? "bg-[#1d1d1d]" : "bg-white"
   }`}>
       <h2 className={`xl:mt-0 sm:mt-0 md:mt-0 lg:mt-0 text-sm sm:text-lg md:text-lg xl:text-xl font-light text-center ${darkMode ? "text-white" : "text-black"}`}>
